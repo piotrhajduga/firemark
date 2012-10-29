8a361460e01f702142a221cf87e471c4e90e24b6
Piotr Hajduga <kosarock@gmail.com>
2012-10-29 02:36:37 +0100

    Refactoring + logging added

--- actions/location.py
+++ actions/location.py
@@ -1,3 +1,4 @@
+import logging
 from twisted.web.resource import Resource
 from twisted.web.error import NoResource
 import json
@@ -10,9 +11,14 @@ class Location(Resource):
         self.output = output
 
     def render_GET(self, request):
+        logging.debug('Get request: %s', request)
         if self.output == 'JSON':
-            return json.dumps(self.__dict__)
+            result = json.dumps(self.__dict__)
+            logging.debug('Returning json object as a result: %s', result)
+            return result
+        logging.warn('Speaking only json at the time')
         return NoResource()
 
     def getChild(self, name, request):
+        logging.warn('No children to this page')
         return self
--- run.py
+++ run.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python2.7
+import logging
 from twisted.web.resource import Resource
 from twisted.web.server import Site
 from twisted.internet import reactor
 import actions.location
 
 
+log_format = '%(asctime)s %(levelname)s [%(module)s %(lineno)d %(funcName)s] %(message)s'
+logging.basicConfig(level='DEBUG', format=log_format)
+
 jsonResource = Resource()
 jsonResource.putChild('location', actions.location.Location(output='JSON'))
 
 root = Resource()
 root.putChild('json', jsonResource)
+
 factory = Site(root)
+logging.info('Listening on the port %d', 8880)
 reactor.listenTCP(8880, factory)
+logging.info('Running the instance')
 reactor.run()
