b752e3db9fc2459ef47535a97686fb113ed89b96
Piotr Hajduga <kosarock@gmail.com>
2012-11-22 15:28:03 +0100

    Added static content serving

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,7 +1,10 @@
 from twisted.web.resource import Resource
+from twisted.web.static import File
 from location import Location
+
 
 def get_app_resource(mongodb):
     resource = Resource()
     resource.putChild('location', Location(mongodb))
+    resource.putChild('static', File('static'))
     return resource
