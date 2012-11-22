d7e3d229e3664f1860914cd77851dcad2e9fce16
Piotr Hajduga <kosarock@gmail.com>
2012-11-22 14:53:33 +0100

    Working templating using Mako

--- actions.py
+++ actions.py
@@ -1,6 +1,7 @@
 import logging
 from twisted.web.resource import Resource
 from twisted.web.error import NoResource
+from mako.template import Template
 import json
 import util
 
@@ -12,43 +13,53 @@ def get_output_type_from_request(request):
         return 'HTML'
 
 
-class Renderer(object):
-    renderers = {}
-
-    def get_output(self, type_key, data):
-        try:
-            render = self.renderers[type_key]
-            return render(data)
-        except KeyError:
-            logging.exception('Cannot find renderer for type %s', type_key)
-            return None
-
-
 class Location(Resource):
-    renderer = Renderer()
+    template_HTML = Template(filename='templates/location.html')
 
     def __init__(self, mongodb):
         Resource.__init__(self)
         self.locations = mongodb.locations
         self.players = mongodb.players
-        self.renderer.renderers['HTML'] = lambda data: str(data)
-        self.renderer.renderers['JSON'] = json.dumps
 
     def render_GET(self, request):
         type_key = get_output_type_from_request(request)
-        login = util.User(request.getSession()).login
-        if login is None:
-            logging.warn('User not logged in!')
-            return self.renderer.get_output(type_key, util.NOT_LOGGED_IN)
-        logging.info('Logged user: %s', login)
-        player = self.players.find_one({'login': login})
-        location = self.locations.find_one({'_id': player['location_id']})
-        logging.debug('Trying to render %s string as a result', type_key)
-        return self.renderer.get_output(type_key, data=location)
+        if type_key == 'JSON':
+            return self.render_JSON(request)
+        else:  # type_key == 'HTML'
+            return self.render_HTML(request)
+
+    def render_HTML(self, request):
+        try:
+            player = self.get_player(request)
+            location = self.get_location(player)
+            data = {'location': location, 'player': player}
+        except UserWarning:
+            data = {'errno': 11, 'error': 'Not logged in!'}
+        return self.template_HTML.render(**data).encode('utf-8')
+
+    def render_JSON(self, request):
+        try:
+            player = self.get_player(request)
+            location = self.get_location(player)
+            data = {'location': location}
+        except UserWarning:
+            data = {'errno': 11, 'error': 'Not logged in!'}
+        return json.dumps(data)
 
     def getChild(self, name, request):
         logging.warn('No children to this page')
         return NoResource()
+
+    def get_player(self, request):
+        login = util.User(request.getSession()).login
+        if login is None:
+            logging.warn('User not logged in!')
+            raise UserWarning('Not logged in!')
+        logging.debug('Logged user: %s', login)
+        return self.players.find_one({'login': login})
+
+    def get_location(self, player):
+        return self.locations.find_one({'_id': player['location_id']})
 
 
 # actions resources resource
