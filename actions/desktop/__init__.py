f44c8f941f513fba8b5aab7c60a54b33df3ebdd2
Piotr Hajduga <kosarock@gmail.com>
2013-01-24 15:21:46 +0100

    Bugfix: import problem from previous commit
    imports should be placed after defining name imported by imported
    modules!

--- actions/desktop/__init__.py
+++ actions/desktop/__init__.py
@@ -2,15 +2,17 @@ from twisted.web.resource import Resource
 from engine.user import UserService
 from engine.location import LocationService
 from mako.lookup import TemplateLookup
+
+
+tpl_lookup = TemplateLookup(directories=['templates/desktop'],
+        output_encoding='utf-8', encoding_errors='replace')
+
+
 # controllers for this package
 from location import Location
 from user import User
 from home import HomePage
 from builder import Builder
-
-
-tpl_lookup = TemplateLookup(directories=['templates/desktop'],
-        output_encoding='utf-8', encoding_errors='replace')
 
 
 def get_app_resource(db, config):
