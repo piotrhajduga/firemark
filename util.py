10f5af8676d63366903fe0a8737b932476ef00d5
Piotr Hajduga <kosarock@gmail.com>
2013-01-24 14:52:13 +0100

    Removed unused code from util.py

--- util.py
+++ util.py
@@ -4,17 +4,11 @@ from twisted.web.server import Session as TwistedSession
 from mako.lookup import TemplateLookup
 
 
-def enum(*sequential, **named):
-    enums = dict(zip(sequential, range(len(sequential))), **named)
-    reverse = dict((value, key) for key, value in enums.iteritems())
-    enums['reverse_mapping'] = reverse
-    return type('Enum', (), enums)
-
-
 class Session(Interface):
     user = Attribute('Logged in user')
     errno = Attribute('Error code or 0 if no error')
     error = Attribute('Error message if error occured')
+    data = Attribute('Additional data to hold in session if needed')
 
 
 class SessionImpl(object):
@@ -24,6 +18,7 @@ class SessionImpl(object):
         self.user = None
         self.errno = 0
         self.error = None
+        self.data = None
 
 
 registerAdapter(SessionImpl, TwistedSession, Session)
