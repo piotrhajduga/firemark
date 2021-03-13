4b0353a37241aab4444e91fb2f9c1708ff8c456b
Piotr Hajduga <piotr.hajduga@gmail.com>
2021-03-13 10:29:19 +0100

    Tweaks to url declarations

--- firemark_django/api/urls.py
+++ firemark_django/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
+from django.urls import path
+
 from locations.urls import urlpatterns as locations_urls
 from game.views import (
     GameAPIView,
 )
 
 urlpatterns = [
-    url(r'game/?', GameAPIView.as_view()),
+    path('game/', GameAPIView.as_view()),
     url(r'', include(locations_urls)),
 ]
--- firemark_django/game/urls.py
+++ firemark_django/game/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
+from django.urls import path
+
 from locations.urls import urlpatterns as locations_urls
 from game.views import (
     GameView, GameMainWidgetView,
 )
 
 urlpatterns = [
-    url('main', GameMainWidgetView.as_view()),
-    url('', GameView.as_view()),
+    path('main', GameMainWidgetView.as_view()),
+    path('', GameView.as_view()),
 ]
